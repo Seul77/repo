39a758b4f720928c0c98c0debe9b515590f0d8e6
Seul77 <a01714080@tec.mx>
2025-09-12 21:41:57 -0600

    Update Codigo botica de los pescados AVANCE 4.py

--- Codigo botica de los pescados AVANCE 2.py
+++ Codigo botica de los pescados AVANCE 2.py
@@ -1,6 +1,10 @@
-primero = input("Buen dia, escriba '1' para registrar productos en el sistema, escriba '2' para revisar disponibilidad de productos, escriba '3' para hacer el reporte de ventas: ")
+primero = input("Buen dia, escriba 'registro' para registrar productos en el sistema, escriba 'revision' para revisar disponibilidad de productos, escriba 'reporte' para hacer el reporte de ventas: ")
+
+##en el avance anterior me confundi, puse condicionales en lugar de funciones, para cuando entregue el avance final ya tendra las funciones pero por ahora tengo los condicionales de if.
+
+
         
-if primero == "1":
+if primero == "registro":
     p = input("Muy bien, por favor introduzca el nombre del articulo: ")
     prod = input("Ok, presione '5' si este articulo ya estaba en el inventario, presione '6' si es nuevo y no habia en inventario: ")
     if prod == "5":
@@ -13,36 +17,37 @@ if primero == "1":
         z = vi + x
     print ("¡Gracias!, ahora la cantidad actual de '"+p+"' es de ", z, ".")
 
-elif primero == "2":
-#en este paso aun no se como hacer para definir que "z" es el valor que se da antes
-    q = input("Claro, introduzca el nombre del articulo: ")
-#aqui igual no se como hacer para que con solo el nombre, el programa sepa que articulo es y con base a su nombre y los datos ya antes dados sepa cuanto queda y asi hacer las operaciones
-    qq = z
-    if qq==0:
-        print(q, "agotado.")
-    elif qq > 0 and qq <=10:
-        print("Solo quedan", z,"unidades, hacer pedido urgente.")
-    elif qq>=11 and qq<=25:
-        print("Quedan", z,"unidades, hacer pedido es recomendado.")
-    elif qq>25:
-        print("Quedan", z,"unidades. Hacer pedido no es necesario")
-    
-elif primero == "3":
-    pp = input("¡Claro!, por favor introduzca el nombre del articulo: ")
-    xx = int(input("Gracias, ahora introduzca la cantidad de '"  + pp + "' que se vendio esta semana: "))
-    zz = z - xx
-#aqui tampoco se como hacer para que me defina que z ya la di antes, como un guardado de datos.
-    if zz < 0:
-        print ("Hmm, el numero de ventas es mayor al numero de articulos en el inventario. Revisa bien los datos e intentalo de nuevo.")
-    elif zz==0:
-        print ("¡El producto '",pp,"' esta agotado! Solo hay ", zz, ",es necesario hacer pedido.")
-    elif zz > 0 and zz <=10:
-        print ("¡Gracias!, ahora la cantidad actual de '",pp,"' es de ", zz, ", se requiere hacer pedido.")
-    elif zz >=11 and z <=20:
-        print ("¡Gracias!, ahora la cantidad actual de '",pp,"' es de ", zz, ", hacer pedido es recomendado")
-    elif zz>20:
-        print ("¡Gracias!, ahora la cantidad actual de '",pp,"' es de ", zz, ". Hacer pedido no es necesario.")
-    
+elif primero == "revision":
+
+        q = input("Claro, introduzca el nombre del articulo: ")
+        qq=10
+        if qq==0:
+            print(q, "agotado.")
+        elif qq > 0 and qq <=10:
+            print("Solo quedan", qq,"unidades, hacer pedido urgente.")
+        elif qq>=11 and qq<=25:
+            print("Quedan", qq,"unidades, hacer pedido es recomendado.")
+        elif qq>25:
+            print("Quedan", qq,"unidades. Hacer pedido no es necesario")
+        
+elif primero == "reporte":
+        z = 10
+        pp = input("¡Claro!, por favor introduzca el nombre del articulo: ")
+        xx = int(input("Gracias, ahora introduzca la cantidad de '"  + pp + "' que se vendio esta semana: "))
+        zz = z - xx
+        if zz < 0:
+            print ("Hmm, el numero de ventas es mayor al numero de articulos en el inventario. Revisa bien los datos e intentalo de nuevo.")
+        elif zz==0:
+            print ("¡El producto '",pp,"' esta agotado! Solo hay ", zz, ",es necesario hacer pedido.")
+        elif zz > 0 and zz <=10:
+            print ("¡Gracias!, ahora la cantidad actual de '",pp,"' es de ", zz, ", se requiere hacer pedido.")
+        elif zz >=11 and z <=20:
+            print ("¡Gracias!, ahora la cantidad actual de '",pp,"' es de ", zz, ", hacer pedido es recomendado")
+        elif zz>20:
+            print ("¡Gracias!, ahora la cantidad actual de '",pp,"' es de ", zz, ". Hacer pedido no es necesario.")
+        
 
 else:
     print ('Hmm, tal vez lo escribiste mal, revisa tu ortografia e intentalo de nuevo.')
+
+
